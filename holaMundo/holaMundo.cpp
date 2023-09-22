c2bee22d34ce2c914f3365646f32b608e204faac
rodolfino121 <rodolfodavila706@gmail.com>
2023-09-22 13:09:29 -0600

    Primer commit

---
+++ holaMundo/holaMundo.cpp
@@ -0,0 +1,98 @@
+/*
+#include <iostream>
+#include <box2d/box2d.h>
+
+int main(){
+    //creacion del mundo y asignacion de la gravedad
+    b2Vec2 gravity(0.0f,-9.81f);
+    b2World world(gravity);
+
+    //caracteristicas del cuerpo
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f,-10.0f);
+
+    //creamos el cuerpo
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    //crear su forma
+    b2PolygonShape groundBox;
+    groundBox.SetAsBox(50.0f,1.0f);
+
+    groundBody ->CreateFixture(&groundBox,0.0f);
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f,20.0f);
+
+    b2Body* body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f,0.0f);
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density=1.0f;
+    fixtureDef.friction=0.3f;
+
+    body ->CreateFixture(&fixtureDef);
+
+    float timeStep = 1.0f/60;
+
+    int32 velocityIterations = 6;
+    int32 positionIterations =2;
+
+    for (int32 i=0;i<60;++i){
+        world.Step(timeStep, velocityIterations, positionIterations);
+        b2Vec2 position = body->GetPosition();
+        float angle = body ->GetAngle();
+        printf("%4.2f %4.2f %4.2f\n",position.x,position.y,angle);
+    }
+}
+*/
+#include <iostream>
+#include <box2d/box2d.h>
+
+int main() {
+    //Creacion del mundo y de la gravedad
+    b2Vec2 gravity(0.0f, -9.81f);
+    b2World world(gravity);
+
+    //Caracteristicas del cuerpo
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f, -10.0f);
+    //Creamos el cuerpo
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    //Crear la forma
+    b2PolygonShape groundBox;
+    groundBox.SetAsBox(50.0f,1.0f);
+
+    groundBody ->CreateFixture(&groundBox, 0.0f);
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f, 20.f);
+    b2Body* body = world.CreateBody(&bodyDef);
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f,1.0f);
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+
+    body ->CreateFixture(&fixtureDef);
+
+    float timeStep = 1.0f/60.0f;
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
+
+    for (int32 i = 0; i < 60; ++i)
+    {
+        world.Step(timeStep, velocityIterations, positionIterations);
+        b2Vec2 position = body->GetPosition();
+        float angle = body->GetAngle();
+        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+
+
+    }
+}
